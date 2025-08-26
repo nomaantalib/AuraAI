e72b691d81cf76b62112e7ff99b3b7e2cc0890a4
nomaantalib <mohdnomaantalib@gmail.com>
2025-08-26 20:14:42 +0530

    Frontend : sidebar UI enhancement done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,9 @@
 import React from 'react'
-import { assets } from './assets/assets.js'
+import Sidebar from './components/sidebar/sidebar.jsx'
 
 const App = () => {
   return (
- <div className="sidebar">
-    <div className="top">
-<img src={assets.menu_icon} alt="" />
-<div className="new-chat">
-    <img src={assets.plus_icon} alt="" />
-    <p>New chat</p>
-
-</div>
-<div className="recent">
-    <p className="recent-title">Recent</p>
-    <div className="recent-entry">
-        <img src={assets.message_icon} alt="" />
-        <p>What is Reactjs ?.....</p>
-        
-    </div>
-</div>
-
-<div className="bottom">
-    <div className="bottom-item recent-entry">
-        <img src={assets.question_icon} alt="" />
-        <p>Help</p>
-    </div>
-       <div className="bottom-item recent-entry">
-        <img src={assets.history_icon} alt="" />
-
-        <p>Activities</p>
-    </div>
-       <div className="bottom-item recent-entry">
-        <img src={assets.setting_icon} alt="" />
-        <p>Settings </p>
-    </div>
-</div>
-    </div>
- </div>
+    <Sidebar />
   )
 }
 
---
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { assets } from '../../assets/assets.js'
+import './sidebar.css'
+
+
+const Sidebar = () => {
+
+const [extended , setExtended] = React.useState(true)
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img className="menu" onClick={()=>setExtended(prev=>!prev)} src={assets.menu_icon} alt="" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+         <p>New chat</p>
+        </div>
+       
+        <div className="recent">
+          <p className="recent-title">Recent</p>
+          <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is Reactjs ?.....</p>
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+      <div className="bottom">
+       
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended?<p>Help</p>:null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended?<p>Activities</p>:null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended?<p>Settings</p>:null}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
